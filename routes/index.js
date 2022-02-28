e66a656b8bc5e7c2e56077cc7cb2ab328fc659a0
Om Prakash Tiwari <optiwari.india@gmail.com>
2022-02-28 12:12:13 +0530

    2022-02-28-1212

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const twig = require('twig');
-
+let captchakey="6LemN6UeAAAAAMqv3WRb0kGJ4kKZWN374FVW2z7-";
 const router = require('express').Router();
 router.route("/")
     .get((req, res) => {
         
-        res.render("index");
+        res.render("index",{captchakey:captchakey});
     })
     .post((req, res) => {
         res.send("post");
