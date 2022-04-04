25c35af950f56c8eda40d45faba80b97d0c20867
Om Prakash Tiwari <optiwari.india@gmail.com>
2022-04-04 13:35:18 +0530

    2022-04-04-1335

--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const session = require("express-session");
 const { append } = require("express/lib/response");
 const mongoose = require("mongoose");
 const fs = require("fs");
+const compression = require("compression");
 let publicDir;
 if (fs.existsSync("./public"))
-     publicDir = __dirname + "/public";
+    publicDir = __dirname + "/public";
 else
-     publicDir = __dirname.split("/").slice(0, -1).join("/") + "/public";
+    publicDir = __dirname.split("/").slice(0, -1).join("/") + "/public";
 
 // console.log(publicDir);
 
 app = express();
 mongoose.connect(process.env.mongodb, { useNewUrlParser: true })
     .then(() => {
+        app.use(compression())
         app.set('view engine', "twig");
         if (process.env.mode == "dev") {
             const livereload = require("livereload");
@@ -28,14 +30,16 @@ mongoose.connect(process.env.mongodb, { useNewUrlParser: true })
             })
             app.use(connectLivereload());
         }
-        app.use(express.static(publicDir))
+        app
+            // .use(compression({ threshold: 0 }))
+            .use(express.static(publicDir))
             .use(express.urlencoded({ extended: true }))
             .use(express.json())
             .use(session({ secret: 't2o', resave: false, saveUninitialized: false, cookie: { maxAge: 60 * 60 * 24 * 30, secure: false } }))
             .use(logs)
             .use((req, res, next) => {
-                if(req.session.passport){
-                    info={provider:req.session.passport.user.provider,id:req.session.passport.user.id};
+                if (req.session.passport) {
+                    info = { provider: req.session.passport.user.provider, id: req.session.passport.user.id };
                     console.log(info);
                 }
                 next();
@@ -54,7 +58,7 @@ mongoose.connect(process.env.mongodb, { useNewUrlParser: true })
                 console.log("Server started on port " + process.env.port);
             });
     })
-const logs=(req,res,next)=>{
+const logs = (req, res, next) => {
     // console.log([req.method,req.url||{},req.session||{},req,body||{}]);
     next();
 }
