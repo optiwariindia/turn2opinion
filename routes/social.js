ef485e068666cf8f3213336c256efb2dac0789cd
Om Prakash Tiwari <optiwari.india@gmail.com>
2022-04-14 16:09:24 +0530

    2022-04-14-1609

--- routes/social.js
+++ routes/social.js
@@ -1,6 +1,6 @@
-const router=require("express").Router();
+const router = require("express").Router();
 const mongoose = require('mongoose');
-const passport=require("passport");
+const passport = require("passport");
 const User = mongoose.model("users", require("../modals/user"));
 
 passport.serializeUser((user, done) => {
@@ -12,31 +12,31 @@ passport.deserializeUser((user, done) => {
 
 router.use(passport.initialize());
 router.use(passport.session());
-router.get("/confirm",async(req,res)=>{
-    if(!("session" in req && "passport" in req.session && "user" in req.session.passport))
+router.get("/confirm", async (req, res) => {
+    if (!("session" in req && "passport" in req.session && "user" in req.session.passport))
         return res.redirect("/");
-    auth={
-        provider:req.session.passport.user.provider,
-        id:req.session.passport.user.id
+    auth = {
+        provider: req.session.passport.user.provider,
+        id: req.session.passport.user.id
     };
     try {
-        user=await User.find({"social.provider":auth.provider,"social.id":auth.id});
-        if(user.length == 0){
-            user=await User.find({email:req.session.passport.user.emails[0].value});
-            if(user.length ==0)
+        user = await User.find({ "social.provider": auth.provider, "social.id": auth.id });
+        if (user.length == 0) {
+            user = await User.find({ email: req.session.passport.user.emails[0].value });
+            if (user.length == 0)
                 return res.redirect("/");
-                req.session.user=user[0];
-                req.session.user.social.push(auth);
-            await req.session.user.save();
-            res.redirect("/user/dashboard");
-        }        
+        }
+        req.session.user = user[0];
+        req.session.user.social.push(auth);
+        await req.session.user.save();
+        res.redirect("/user/dashboard");
     } catch (error) {
         res.json(error);
     }
 })
-router.use("/facebook",require("./social/facebook"));
-router.use("/twitter",require("./social/twitter"));
-router.use("/linkedin",require("./social/linkedin"));
-router.use("/google",require("./social/google"));
+router.use("/facebook", require("./social/facebook"));
+router.use("/twitter", require("./social/twitter"));
+router.use("/linkedin", require("./social/linkedin"));
+router.use("/google", require("./social/google"));
 
-module.exports=router;
+module.exports = router;
